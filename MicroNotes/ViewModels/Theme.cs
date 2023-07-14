163e7e8d1f77e44e95fab5c27c34fd0653d1d85d
koglerch13 <christiankogler@outlook.com>
2023-07-14 14:53:07 +0200

    full theming support
    with a "fluent" theme to start

--- MicroNotes/ViewModels/Theme.cs
+++ MicroNotes/ViewModels/Theme.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Media;
 
 namespace MicroNotes.ViewModels;
@@ -23,13 +24,32 @@ public static class Theme
     public static Brush ContentTextSelectionBackgroundColor { get; } = new SolidColorBrush(Colors.LightBlue);
     public static Brush ContentTextSelectionColor { get; } = new SolidColorBrush(Colors.Black);
     
-
     public static Brush ListItemTextColor { get; } = new SolidColorBrush(Colors.Black);
     public static Brush ListItemBackgroundColor { get; } = new SolidColorBrush(Colors.White);
     
     public static Brush ListItemTextColorHover { get; } = new SolidColorBrush(Colors.Black);
-    public static Brush ListItemBackgroundColorHover { get; } = new SolidColorBrush(Colors.LightGray);
+    public static Brush ListItemBackgroundColorHover { get; } = new SolidColorBrush(Colors.LightBlue);
 
     public static Brush ListItemTextColorSelected { get; } = new SolidColorBrush(Colors.Black);
-    public static Brush ListItemBackgroundColorSelected { get; } = new SolidColorBrush(Colors.Gray);
+    public static Brush ListItemBackgroundColorSelected { get; } = new SolidColorBrush(Colors.SkyBlue);
+    
+    public static Thickness ListItemMargin { get; } = new Thickness(0);
+    public static Thickness ListItemPadding { get; } = new Thickness(10,10);
+    public static CornerRadius ListItemRadius { get; } = new CornerRadius(0);
+
+    public static double ListItemFontSize { get; } = 16;
+    public static double TitleFontSize { get; } = 20;
+    public static double TextFontSize { get; } = 12;
+
+    public static double PlaceholderFontSize { get; } = 18;
+    public static Brush PlaceholderTextColor { get; } = new SolidColorBrush(Colors.Black);
+
+    public static CornerRadius MenuButtonRadius { get; } = new CornerRadius(0);
+    public static double MenuButtonHeight { get; } = 24;
+    public static double MenuButtonWidth { get; } = 24;
+    public static Thickness MenuButtonPadding { get; } = new Thickness(0);
+    public static Thickness MenuButtonMargin { get; } = new Thickness(4,4,0,4);
+
+    public static Thickness TitlePadding { get; } = new Thickness(8,4,0,8);
+    public static Thickness TextPadding { get; } = new Thickness(8,0,8,0);
 }
